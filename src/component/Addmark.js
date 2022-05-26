1503b3fd35db5d59307b9944bf543c8d9a92fdf0
vaisakhg0 <vaisakhg30@gmail.com>
2022-05-26 11:29:54 +0530

    mark

--- src/component/Addmark.js
+++ src/component/Addmark.js
@@ -1,8 +1,34 @@
 import React from 'react'
 
 const Addmark = () => {
+    
+
   return (
-    <div>Addmark</div>
+    <div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+        <div class="row g-3">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label for="" class="form-label">Name</label>
+                <input type="text" class="form-control"/>
+            </div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label for="" class="form-label">Admn no</label>
+                <input type="text" class="form-control"/>
+            </div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label for="" class="form-label">cgpa</label>
+                <input type="text" class="form-control"/>
+            </div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button class="btn btn-success">SEARCH</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+        
+
   )
 }
 
